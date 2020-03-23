cef3eefbe210f3f218dee95de7d7ad5907311e0e
Sajantoor98 <Sajantoor98@gmail.com>
2020-03-23 11:07:05 -0700

    ⌨  added typing effect

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,6 +10,10 @@ class Landing extends React.Component {
     this.state = {
       subtitle: ["Programmer", "Designer", "Student"],
       index: 0,
+      currentWord: null,
+      wordIndex: 0,
+      finishedWord: false,
+      timeout: 0,
     }
   }
 
@@ -18,7 +22,7 @@ class Landing extends React.Component {
       <div className="landing">
         <div className="title-contain">
           <h1> Sajan Toor </h1>
-          <h2> I'm a <span className="highlight"> {this.state.subtitle[this.state.index]}. </span> </h2>
+          <h2> I'm a <span className="highlight"> {this.state.currentWord}. </span> </h2>
         </div>
         <img src={mountainImg} alt=""/>
       </div>
@@ -27,19 +31,47 @@ class Landing extends React.Component {
 
 
   componentDidMount() {
-    const this_ = this;
-    let index = this_.state.index;
+    setInterval(this.typing, 100, this);
+  }
 
-    setInterval(function() {
-      if (index === (this_.state.subtitle.length - 1)) {
+  typing(_this) {
+    const this_ = _this;
+    let index = this_.state.index;
+    let word = this_.state.subtitle[index];
+    let wordLength = this_.state.subtitle[index].length;
+    let wordIndex = this_.state.wordIndex;
+    let currentWorld = this_.state.currentWord;
+    let finishedWord = this_.state.finishedWord;
+    let timeout = this_.state.timeout;
+
+    if ((wordLength >= wordIndex) && !finishedWord) {
+      wordIndex++;
+    }
+
+    if ((wordIndex - 1) === wordLength) {
+      timeout++;
+      if (timeout === 10) {
+        finishedWord = true;
+      }
+    }
+
+    if ((wordIndex > 0) && finishedWord) {
+      wordIndex--;
+    }
+
+    if ((wordIndex === 0) && finishedWord) {
+      if (index === 2) {
         index = 0;
       } else {
         index++;
       }
+      timeout = 0;
+      finishedWord = false;
+    }
 
-      this_.setState({index: index});
+    currentWorld = word.substring(0, wordIndex);
 
-    }, 1000)
+    this_.setState({currentWord: currentWorld, wordIndex: wordIndex, finishedWord: finishedWord, index: index, timeout: timeout});
   }
 }
 
