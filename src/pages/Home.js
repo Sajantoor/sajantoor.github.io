ba5ce8ad25376a522525776d655e484646fb2b57
Sajantoor98 <Sajantoor98@gmail.com>
2020-07-23 14:12:36 -0700

    :construction: Before rewriting CSS for projects page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ class Home extends React.Component {
         <Landing/>
         <About/>
         <Projects
-          title="Coronavirus Tracker" 
+          title="VEX Robotics Competition" 
           subtitle="Tracking the COVID-19 Pandemic"
           img="https://images.unsplash.com/photo-1550645612-83f5d594b671?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
           caption="Visualizing and making COVID-19 information accessible"
