f5364f10c63a2ceb6c19c325b3856642c1dc0ed4
Sajantoor98 <Sajantoor98@gmail.com>
2020-07-28 17:45:55 -0700

    :sparkles: Created URL anchors and scrolling to element on nav click

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <Router>
       <div className="App">
-        <Switch>
-          <Route path={["/", "/home",]} exact component={Home}></Route>
-        </Switch>
+        <Home/>
       </div>
-    </Router>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,6 +13,16 @@ class Navigation extends React.Component {
 
   componentDidMount() {
     window.addEventListener('scroll', this.checkScroll);
+    // url pathname
+    const pathname = window.location.pathname;
+    // on load with url, go to location
+    if (pathname === '/about') {
+      this.goTo(2);
+    } else if (pathname === '/projects') {
+      this.goTo(3);
+    } else {
+      this.goTo(1);
+    }
   }
 
   checkScroll() {
@@ -26,22 +36,50 @@ class Navigation extends React.Component {
       // if element is in view
       if (elementTopPos < 0 && elementBottomPos >= 0) {
         // prevent rerendering over and over
-        if (this.state.scrollPosition !== i) {
+        if (this.state.scrollPosition !== i) {      
           this.setState({scrollPosition: i})
-        }
-        // TODO: Change url ?
+          // changes url based off position
+          if (i === 1) {
+            window.history.replaceState({}, '', 'home');
+          } else if (i === 2) {
+            window.history.replaceState({}, '', 'about');
+          } else if (i === 3) {
+            window.history.replaceState({}, '', 'projects');
+          }
+        } 
 
         break;
       }
     }
   }
+  
+  // scrolls to a certain element
+  goTo(pos) {
+    let thisElement = this.refs.navigation; 
+    let siblings = thisElement.parentNode.children;
+    let plusConstant; // constant used so user sees all the content needed based on element
+    // depending on element index
+    if (pos === 2) {
+      plusConstant = 30;
+      window.history.replaceState({}, '', 'about');
+    } else if (pos === 3) {
+      plusConstant = 150;
+      window.history.replaceState({}, '', 'projects');
+    } else {
+      plusConstant = 0;
+      window.history.replaceState({}, '', 'home');
+    }
+    // ssmoth scrolls based on scroll distance to element
+    const scrollDistance = siblings[pos].getBoundingClientRect().top + window.scrollY + plusConstant;
+    window.scroll({top: scrollDistance, behavior: 'smooth'});
+  }
 
   render() {
     return(
       <div className="navigation" ref="navigation">
-        <button className={(this.state.scrollPosition === 3) ? "navButton focus" : "navButton"}> Projects </button>
-        <button className={(this.state.scrollPosition === 2) ? "navButton focus" : "navButton"}> About </button>
-        <button className={(this.state.scrollPosition === 1) ? "navButton focus" : "navButton"}>  Home </button>
+        <button className={(this.state.scrollPosition === 3) ? "navButton focus" : "navButton"} onClick={() => this.goTo(3)}> Projects </button>
+        <button className={(this.state.scrollPosition === 2) ? "navButton focus" : "navButton"} onClick={() => this.goTo(2)}> About </button>
+        <button className={(this.state.scrollPosition === 1) ? "navButton focus" : "navButton"} onClick={() => this.goTo(1)}>  Home </button>
       </div>
     )
   }
