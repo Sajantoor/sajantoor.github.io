2085c74bbd3f1bb0597c9968327e0e4074f4b15a
Sajan Toor <sajantoor98@gmail.com>
2023-08-29 19:01:54 -0700

    Add resume to website

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ import { ReactComponent as GitHub } from '../assets/logos/GitHub.svg';
 import LinkedIn from '../assets/logos/LinkedIn.png';
 import { ReactComponent as AboutTitleAsset } from '../assets/shapes/AboutTitleAsset.svg';
 import { ReactComponent as TransitionAsset } from '../assets/shapes/Transition Asset.svg';
+import Resume from '../assets/Resume - Sajan Toor.pdf';
 
 class About extends React.Component {
   render() {
@@ -25,7 +26,9 @@ class About extends React.Component {
             <a href="https://www.linkedin.com/in/sajan-toor-3091a11b2" target="_blank" rel="noreferrer"> <img src={LinkedIn} alt="LinkedIn Logo" /> </a>
           </div>
 
-          <button> Download Resume </button>
+          <a href={Resume} download="Resume - Sajan Toor.pdf">
+            <button> Download Resume </button>
+          </a>
         </div>
 
         {/* <ul className="circles">
