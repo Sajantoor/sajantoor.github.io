076e3b470d5b95c1b919661597aec0be0137cc25
Sajan Toor <sajantoor98@gmail.com>
2023-08-29 19:14:19 -0700

    Add resume download path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { useEffect } from 'react';
 import './App.css'
 import './assets/fonts/fonts.css';
 import Home from './pages/Home';
 
 function App() {
+  useEffect(() => {
+    // check if the user has entered "/resume" in the url 
+    const path = window.location.pathname;
+
+    if (path !== "/resume") {
+      return;
+    }
+
+    // Simulate a click on the hidden download link and download the resume
+    const downloadLink = document.createElement('a');
+    downloadLink.href = '../assets/Resume - Sajan Toor.pdf';
+    downloadLink.download = 'Resume - Sajan Toor.pdf';
+    downloadLink.style.display = 'none';
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  }, []);
+
   return (
-      <div className="App">
-        <Home/>
-      </div>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
